ef675b35bd5f91399d7aefe05f2cbdcb582f12ed
Will Spooner <contact@willbspooner.uk>
2026-05-12 12:38:56 +0100

    Implement serial driver

---
+++ kernel/arch/i386/serial.c
@@ -0,0 +1,54 @@
+#include <stdint.h>
+#include <stddef.h>
+#include <string.h>
+
+#define COM1_PORT 0x3F8
+#define SERIAL_DATA          0
+#define SERIAL_INTERRUPT_EN  1
+#define SERIAL_FIFO_CTRL     2
+#define SERIAL_LINE_CTRL     3
+#define SERIAL_MODEM_CTRL    4
+#define SERIAL_LINE_STATUS   5
+
+static inline void outb(uint16_t port, uint8_t val)
+{
+    __asm__ volatile ( "outb %b0, %w1" : : "a"(val), "Nd"(port) : "memory");
+}
+
+static inline uint8_t inb(uint16_t port)
+{
+    uint8_t ret;
+    __asm__ volatile ( "inb %w1, %b0"
+                   : "=a"(ret)
+                   : "Nd"(port)
+                   : "memory");
+    return ret;
+}
+
+void init_serial_port() {
+  // disable interrupts
+  outb(COM1_PORT + SERIAL_INTERRUPT_EN, 0x00);
+
+  // dlab dance
+  outb(COM1_PORT + SERIAL_LINE_CTRL, 0x80);  // access baud divisor
+  outb(COM1_PORT + 0, 0x03); // divisor low byte:  115200 / 3 = 38400 baud
+  outb(COM1_PORT + 1, 0x00); // divisor high byte
+  outb(COM1_PORT + SERIAL_LINE_CTRL, 0x03); // 8 bits, no parity, 1 stop bit
+  outb(COM1_PORT + SERIAL_FIFO_CTRL, 0xC7); // fifo
+  outb(COM1_PORT + SERIAL_MODEM_CTRL, 0x0C); // DTR, RTS, Out 2
+}
+
+void serial_putchar(char c) {
+  while (!(inb(COM1_PORT + SERIAL_LINE_STATUS) & 0x20)) {}
+  outb(COM1_PORT + SERIAL_DATA, c);
+}
+
+void serial_write(const char * data, size_t len) {
+  for (size_t i = 0; i < len; i++) {
+    serial_putchar(data[i]);
+  }
+}
+
+void serial_writestring(const char * data) {
+  serial_write(data, strlen(data));
+}
---
+++ kernel/include/kernel/serial.h
@@ -0,0 +1,9 @@
+#ifndef ARCH_I386_VGA_H
+#define ARCH_I386_VGA_H
+
+void init_serial_port(void);
+void serial_putchar(char c);
+void serial_write(const char * data, size_t len);
+void serial_writestring(const char * data);
+
+#endif // !ARCH_I386_VGA_H
